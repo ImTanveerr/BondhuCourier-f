798ec694c9508d410d3c089477fd4e9dc7cf657b
Demo <bestfriendever742@gmail.com>
2025-08-14 19:08:40 +0600

    updated

---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import App from "@/App";
+import { createBrowserRouter } from "react-router";
+
+export const router =createBrowserRouter([
+    {
+        //element: <App/>,
+        Component:App,
+        path: "/",
+        children: [{
+          
+        }]
+    }
+])
